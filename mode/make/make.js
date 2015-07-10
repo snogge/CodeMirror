cb3cb4f6da9d1bdff6b31b7d1f2ca86aa077d353
Ola Nilsson <ola.nilsson@gmail.com>
2015-07-10 22:41:38 +0200

    [make mode] Add basic simple-mode for make
    Plenty of syntax highlighting work, but there is still work to be done.

---
+++ mode/make/make.js
@@ -0,0 +1,159 @@
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"), require("../../addon/mode/simple"), require("../shell/shell"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror", "../../addon/mode/simple", "../shell/shell"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
+  "use strict";
+
+  function textstates(start, end, chars) {
+    var commonrules = [
+      { regex: /#.*/, token: 'comment', pop: true},
+      { regex: /\$\{/, push: 'curlymacro' },
+      { regex: /\$\(/, push: 'parenmacro' },
+      { regex: /\$./,  token: 'variable'},
+    ];
+    var states = {};
+    states[start] = Array.from(commonrules);
+    states[end] = Array.from(commonrules);
+    states[start].push({regex: new Regex('[^' + chars + ']'), next: end});
+    states[end].unshift({ regex: /^/, sol:true, pop: true});
+    states[end].push({regex: new Regex('[^' + chars + ']*$'), pop:true});
+    states[end].push({regex: new Regex('[^' + chars + ']+')});
+    return states;
+  }
+  
+  CodeMirror.defineSimpleMode("make", {
+    start: [
+      { regex: /#.*/, token: 'comment'},
+      /* conditionals */
+      { sol: true,
+        regex: /(\s*)(ifn?eq)/,
+        token: [null, 'keyword'],
+        push: 'conditional'
+      },
+      { sol: true,
+        regex: /(\s*)(ifdef)(\s+)([^#=:\s]+)/,
+        token: [null, 'keyword', null, 'variable'],
+      },
+      { sol: true,
+        regex: /(\s*)(else|endif)/,
+        token: [null, 'keyword']
+      },
+      /* assigment */
+      { sol: true,
+        regex: /(\s*)([^#=:\s]+)(\s*)((?:[!?+]|::?)?=)/,
+        token: [null, 'variable', null, 'operator'],
+        push: 'text'
+      },
+      /* define*/
+      { sol: true,
+        regex: /(\s*)(define)(\s+)([^#=:\s]+)(\s*)((?:[!?+]|::?)?=)/,
+        token: [null, 'keyword', null, 'variable', null, 'operator'],
+        push: 'define'
+      },
+      { sol: true,
+        regex: /(\s*)(define)(\s+)([^#=:\s]+)/,
+        token: [null, 'keyword', null, 'variable'],
+        push: 'define'
+      },
+      { sol: true,
+        regex: /(\s*-?)(include)(\s+)/,
+        token: [null, 'keyword', null],
+        push: 'starttext'
+      },
+      // target
+      { sol:true, regex: /(\s*)(.*?)(\s*)(:)(\s*$)/,
+        token: [null,'def',null,'operator', null] },
+      { sol:true, regex: /(\s*)(.*?)(\s*)(:)/,
+        token: [null,'def',null,'operator'],
+        push: 'startprereq' },
+      // action
+      { sol: true, regex: '(\t\s*)([-+@])', token: [null,'operator'],
+        mode: {spec: 'text/x-sh', endeol: true}},
+      { sol: true, regex: '\t', token: null,
+        mode: {spec: 'text/x-sh', endeol: true}}
+    ],
+    startprereq: [
+      { regex: /(?=;)/, next: 'samelinecmd' },
+      { regex: /#.*/, token: 'comment', pop: true},
+      { regex: /\$\{/, push: 'curlymacro' },
+      { regex: /\$\(/, push: 'parenmacro' },
+      { regex: /\$./,  token: 'variable'},
+      { regex: /[^#$;]/, token: null, next: 'prereq'},
+    ],
+    prereq: [
+      { regex: /^/, sol:true, pop: true},
+      { regex: /(?=;)/, next: 'samelinecmd'},
+      { regex: /#.*/, token: 'comment', pop: true},
+      { regex: /\$\{/, push: 'curlymacro' },
+      { regex: /\$\(/, push: 'parenmacro' },
+      { regex: /\$./,  token: 'variable'},
+      { regex: /[^#$;]*$/, pop: true},
+      { regex: /[^#$;]+/, token: null},
+    ],
+    samelinecmd: [
+      { regex: /;/, token: 'operator',
+        mode: {spec: 'text/x-sh', endeol: true} },
+      { regex: /^/, pop: true}
+    ],
+    conditional: [
+      /* Lots of room for improvement here....*/
+      { regex: /\(\s*.*\s*,\s*.*\s*\)/,
+        token: null,
+        pop: true
+      },
+      { regex: /(?:'[^']*'|"[^']*")\s+(?:'[^']*'|"[^']*")/,
+        token: null,
+        pop: true
+      },
+    ],
+    text: [
+      { regex: /^/,       sol:true, pop: true},
+      { regex: /#.*/,     token: 'comment', pop: true},
+      { regex: /\$\{/,    push: 'curlymacro' },
+      { regex: /\$\(/,    push: 'parenmacro' },
+      { regex: /\$./,     token: 'variable'},
+      { regex: /$/,       pop: true},
+      { regex: /[^#$]*$/, pop: true},
+      { regex: /[^#$]+/,  token: null},
+    ],
+    starttext: [
+      { regex: /#.*/, token: 'comment', pop: true},
+      { regex: /\$\{/, push: 'curlymacro' },
+      { regex: /\$\(/, push: 'parenmacro' },
+      { regex: /\$./,  token: 'variable'},
+      { regex: /[^#$]/, token: null, next: 'text'},
+    ],
+    curlymacro: [
+      { regex: /\$\{/, push: 'curlymacro' },
+      { regex: /\$\(/, push: 'parenmacro' },
+      { regex: /(\$)(.)/,  token: [null, 'variable']},
+      { regex: /[^}$]+/, token: 'variable'},
+      { regex: /\}/, pop: true}
+    ],
+    parenmacro: [
+      { regex: /\$\{/, push: 'curlymacro' },
+      { regex: /\$\(/, push: 'parenmacro' },
+      { regex: /\$./,  token: 'variable'},
+      { regex: /[^)$]+/, token: 'variable'},
+      { regex: /\)/, pop: true}
+    ],
+    define: [
+      {sol: true, regex: /(\s*)(endef)/, token: [null, 'keyword'], pop: true},
+      {regex: /^/, push: 'starttext'}
+    ],
+    meta: {
+      lineComment: '#'
+    }
+  });
+
+  CodeMirror.defineMIME("text/x-make", "make");
+});
+
+// Local Variables:
+// indent-tabs-mode: nil
+// js-indent-level: 2
+// End:
---
+++ mode/make/test.js
@@ -0,0 +1,95 @@
+(function () {
+  var mode = CodeMirror.getMode({}, 'make');
+  function MT(name) { test.mode(name, mode, Array.prototype.slice.call(arguments, 1)); }
+
+  MT("comment",
+     "[comment # a several words long comment]",
+     "not comment [comment # a several words long comment]");
+  
+  MT("ifeq_parens",
+     "[keyword ifneq] (lhs,rhs)",
+     "[keyword ifneq] 'lhs' 'rhs'",
+     "[keyword ifneq] 'lhs' \"rhs\"",
+     "[keyword ifneq] \"lhs\" 'rhs'",
+     "[keyword ifneq] \"lhs\" \"rhs\"",
+     "[keyword ifeq] (lhs,rhs)",
+     "[keyword ifeq] 'lhs' 'rhs'",
+     "[keyword ifeq] 'lhs' \"rhs\"",
+     "[keyword ifeq] \"lhs\" 'rhs'",
+     "[keyword ifeq] \"lhs\" \"rhs\""
+    );
+
+  MT("ifdef",
+     "  [keyword ifdef] 	[variable snookums]",
+     "  [keyword ifdef] 	[variable snookums_${flibbertigibbet}]"
+    );
+  
+  MT("else_and_endif",
+     "[keyword else]",
+     "[keyword endif]");
+
+  MT("assignment",
+     "[variable name] [operator =] some text or other",
+     "[variable name] [operator ?=] some ${[variable text]} or other",
+     "[variable @%/] [operator :=] some $([variable text]${[variable frobnic]}) or other",
+     "[variable foo_$(bar)] [operator !=] some text or $([variable other])",
+     "[variable $(baz)_foo)] [operator ::=] some text or other");
+
+  MT("define",
+     "[keyword define] [variable name]",
+     "some text or other ${[variable foo]} and some more $([variable text])",
+     "even more $([variable text])",
+     "$([variable and]) a third line",
+     "[keyword endef] [comment # comment]");
+
+  MT("define_op",
+     "[keyword define] [variable name] [operator :=] [comment # comment]",
+     "some text or other ${[variable foo]} and some more $([variable text])",
+     "[keyword endef]");
+
+  MT("include",
+     "[keyword include] foo",
+     "  [keyword include] foo",
+     "-[keyword include] foo",
+     "  -[keyword include] foo");
+
+  MT("target",
+     "[def foo bar baz] [operator :]",
+     "[def foo bar baz] [operator :] [comment # comment]",
+     "[def foo bar baz] [operator :] text ${[variable variable]} text",
+     "[def foo bar baz] [operator :] text ${[variable variable]} text [comment # comment]");
+  MT("simple_rule",
+     "[def foo][operator :]",
+     "	[builtin echo] foo");
+  MT("rule",
+     "[def foo bar baz] [operator :]",
+     "	[builtin echo] foo bar baz",
+     "	[operator -][builtin echo] foo bar baz",
+     "	[operator +][builtin echo] foo bar baz",
+     "	[operator @][builtin echo] foo bar baz"
+    );
+  MT("semicolon_target",
+     "[def target][operator :] prereq [operator ;] [builtin echo] command",
+     "	[builtin echo] another command");
+  MT("rule2",
+     "[def edit] [operator :] $([variable objects])",
+     "	[builtin cc] [attribute -o] edit [quote $(objects)]");
+  MT("several_targets",
+     "[def main.o] [operator :] defs.h",
+     "[def kbd.o] [operator :] defs.h command.h",
+     "[def command.o] [operator :] defs.h command.h",
+     "[def display.o] [operator :] defs.h buffer.h",
+     "[def insert.o] [operator :] defs.h buffer.h",
+     "[def search.o] [operator :] defs.h buffer.h",
+     "[def files.o] [operator :] defs.h buffer.h command.h",
+     "[def utils.o] [operator :] defs.h");
+  MT("phony",
+     "[def .PHONY] [operator :] clean",
+     "[def clean] [operator :]",
+     "	[operator -][builtin rm] edit [quote $(objects)]");
+     
+
+
+
+
+})();
